b784dd447e1ab8c808e3378d3c1eae87360e0ab8
Tommy Le <LeTran.Tommy@gmail.com>
2022-07-28 22:31:49 -0500

    Fixed text to make it show up better on page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,12 @@ const Home = () => {
 
   return (
       <div className="home">
-              <h1 className="mainTitle">Top Bars In KC</h1>
+        <h1 className="mainTitle">Top Bars In KC</h1>
+      <div className="HomeImage">
+        <h2>
+          This is just a place holder
+        </h2>
+      </div>
       </div>
   )
 }
